ae669db18e6df32161aba2ec3bb4eccda274f022
kunaly1038 <39200197+kunaly1038@users.noreply.github.com>
2018-07-23 21:58:51 +0530

    Create solution.java

---
+++ java/java_4/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt(); 
+        double d=sc.nextDouble();
+      String s;
+      
+      
+    sc.nextLine();
+      s=sc.nextLine();
+             
+      System.out.println("String: "+s);
+      System.out.println("Double: "+d);
+      System.out.println("Int: "+a);
+    }
+}
