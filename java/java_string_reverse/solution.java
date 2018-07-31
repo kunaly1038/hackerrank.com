7a6e26e5c39bc40ae88f203a8d00dc4df0c8f3b6
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-07-31 21:39:49 +0530

    Create solution.java

---
+++ java/java_string_reverse/solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String reversed="";
+        String A=sc.next();
+        int x=A.length();
+       for(int i=x-1;i>=0;i--)
+       {
+           reversed=reversed+A.charAt(i);
+       }
+        if(A.equals(reversed))
+        {
+            
+            System.out.println("Yes");
+        }
+        else
+            System.out.println("No");
+    }
+}
+
+
+
