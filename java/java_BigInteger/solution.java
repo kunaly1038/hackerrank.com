a5fcc1ac860a49b7d4230ac5b36ef8963354ebf9
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-07-31 21:37:36 +0530

    Create solution.java

---
+++ java/java_BigInteger/solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Solution {
+
+  public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String x,y;
+        x=sc.next();
+        y=sc.next();
+       BigInteger bg=new BigInteger(x);
+               BigInteger bg2=new BigInteger(y);
+System.out.println(bg.add(bg2));
+System.out.println(bg.multiply(bg2));
+    }
+}
